1532e8570199ed5a4e79dd4c141a98d6f3fc488e
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2018-11-13 20:00:03 +0100

    Battery 2 DONE

--- UF1/Bateria02_Streams/Streams2/EX3_EscribirFicheroTexto.java
+++ UF1/Bateria02_Streams/Streams2/EX3_EscribirFicheroTexto.java
@@ -6,5 +6,17 @@ package Bateria02_Streams.Streams2;
 import java.io.*;
 
 public class EX3_EscribirFicheroTexto {
-
+	public static void main (String [] args) throws IOException {
+		File fichero = new File("FicheroTexto.txt");
+		FileWriter fic = new FileWriter (fichero);
+		String prov[] = {"Albacete", "Avila", "Badajoz", "Caceres", "Huelva", "Jaen", "Madrid",
+				"Segovia", "Soria", "Toledo", "Valladolid", "Zamora"};		
+				
+		for (int i = 0; i < prov.length-1; i++) {
+			fic.write(prov[i] + ", "); // Escribimos ciudad a ciudad
+		}
+		
+		fic.append(prov[prov.length-1]); //Añadimos la ultima ciudad (sin coma al final)
+		fic.close ();   // cerramos fichero
+	}
 }
