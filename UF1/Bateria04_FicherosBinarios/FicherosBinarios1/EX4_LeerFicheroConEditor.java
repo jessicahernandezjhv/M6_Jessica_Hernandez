1354971df8a33a60c8969fd870b2fcd43f14627e
Feuer <feuerhv@gmail.com>
2018-11-18 12:16:59 +0100

    Battery 4.1 Exercise 1 done

---
+++ UF1/Bateria04_FicherosBinarios/FicherosBinarios1/EX4_LeerFicheroConEditor.java
@@ -0,0 +1,12 @@
+package Bateria04_FicherosBinarios.FicherosBinarios1;
+
+/* Ejercicio 4. Vuelve a ejecutar los problemas anteriores pero en los pasos intermedios
+trata de leer el fichero “FicheroDatos.dat” con algún editor de texto del
+sistema. ¿Qué pasa? */
+
+import java.io.*;
+
+
+public class EX4_LeerFicheroConEditor {
+
+}
