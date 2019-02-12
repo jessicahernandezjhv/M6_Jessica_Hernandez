33b94a0d4ce80a72f4a7d98fd65149316c972ac6
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2019-02-12 19:27:31 +0100

    Hibernate exercises separated

--- UF2/Bateria04_Hibernate/V/Ex01_VisualizarDepartamento.java
+++
@@ -1,9 +0,0 @@
-package Bateria04_Hibernate.V;
-
-/* EJERCICIo 1. Visualiza el apellido y salario de los empleados 
- * del departamento 20. (Ayuda: la consulta HQL necesaria es: 
- * “from Emple as e where e.depart.deptNo=20”) */
-
-public class Ex01_VisualizarDepartamento {
-
-}
--- UF2/Bateria04_Hibernate/V/Ex02_SenyorArroyo.java
+++
@@ -1,7 +0,0 @@
-package Bateria04_Hibernate.V;
-
-// EJERCICIO 2. Visualiza los datos del señor “ARROYO”
-
-public class Ex02_SenyorArroyo {
-
-}
