b8e21447d7e5538804f8c361b3a37695328a5c07
Jessica Hernandez DAM <cf17jessica.hernandez@iesjoandaustria.org>
2019-05-14 18:45:58 +0200

    UF4 battery 2-3 added files

---
+++ UF4/Bateria02_03_AccesoJavaBeans/Prueba.java
@@ -0,0 +1,17 @@
+package Bateria02_03_AccesoJavaBeans;
+
+
+//import javaBean.Pedido;
+//import javaBean.Producto;
+public class Prueba {
+    public static void main(String[] args) {
+        Producto producto = new Producto(1,"MiProductoEstrella",10,3,16);
+        Pedido pedido =new Pedido();
+        pedido.setProducto(producto);
+        
+        producto.addPropertyChangeListener (pedido);
+        producto.setStockactual(2);
+    }
+}
+
+
